926499c42da7f500640179347307b88126321b43
mumeen <azeezmumeen6@gmail.com>
2022-06-11 09:44:20 +0100

    added contract

--- contracts/contracts/Social.sol
+++ contracts/contracts/Social.sol
@@ -10,16 +10,36 @@ contract Trasaction {
         string userName;
         string twitterHandle;
     }
+    //transaction object
+    struct transactionObject {
+        address senderAddress;
+        string ReceiverAddress;
+        uint256 Amount;
+        string note;
+    }
 
     //users map
     mapping(address => userObject) public userMap;
+
+    //transactions
+    transactionObject[] public transactionsList;
      // adding user on connection with twitter
-    function addUser(
+    function addUser (
         address _address,
         string memory _user,
         string memory _handle
     ) public {
         userMap[_address] = userObject(_address, _user, _handle);
+    }
+
+    //adding txn
+    function addTxn(
+        address _senderAddress,
+        string _ReceiverAddress,
+        uint256 _Amount,
+        string _note
+    ) public{
+        transactionsList.push(transactionObject(_senderAddress, _ReceiverAddress, _Amount, _note));
     }
 
 
