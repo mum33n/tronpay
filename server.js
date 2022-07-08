372f97dcc7eabe9778c6ad990603654e574436d6
bolaji ogidan <bolajiscott@yahoo.com>
2022-07-08 05:39:34 +0100

    setting-up-server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("tron testing!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
