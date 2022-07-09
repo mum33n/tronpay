35e355d0cb9f4490027272bdbde1aeb24ec2ecb2
mumeen <azeezmumeen6@gmail.com>
2022-07-09 14:45:26 +0100

    added google auth

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -3,51 +3,53 @@ import { AiFillTwitterSquare } from "react-icons/ai";
 import banner from "../assets/banner.png";
 import loader from "../assets/Spinner.svg";
 import { useWalletValue } from "../providers/WalletProvider";
-import { FaDiscord, FaShareAlt, FaTwitter } from "react-icons/fa";
-import axios from "axios";
+import Button from "../components/Button";
+import { FaDiscord, FaGoogle, FaShareAlt, FaTwitter } from "react-icons/fa";
+import { signInwithGoogle } from "../utils/discordAuth";
+// import axios from "axios";
 
 function ProfilePage() {
   const { wallet } = useWalletValue();
-  const token = window.location.search.substring(1).split("=")[1];
-  let data = useCallback(() => {
-    let param = new URLSearchParams({
-      client_id: "992266095601909840",
-      client_secret: "v1Hu3p4dCmtX0AJyTEBRQhz-Hxq_ixP3",
-      grant_type: "authorization_code",
-      code: token,
-      redirect_uri: "http://localhost:3000/profile",
-    });
-    return param;
-  }, [token]);
-  const data1 = useMemo(() => data(), [data]);
-  const getToken = useCallback(async () => {
-    let data = await axios
-      .post("https://discord.com/api/oauth2/token", data1, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      })
-      .then((res) => res.data);
-    return data;
-  }, [data1]);
+  // const token = window.location.search.substring(1).split("=")[1];
+  // let data = useCallback(() => {
+  //   let param = new URLSearchParams({
+  //     client_id: "992266095601909840",
+  //     client_secret: "v1Hu3p4dCmtX0AJyTEBRQhz-Hxq_ixP3",
+  //     grant_type: "authorization_code",
+  //     code: token,
+  //     redirect_uri: "http://localhost:3000/profile",
+  //   });
+  //   return param;
+  // }, [token]);
+  // const data1 = useMemo(() => data(), [data]);
+  // const getToken = useCallback(async () => {
+  //   let data = await axios
+  //     .post("https://discord.com/api/oauth2/token", data1, {
+  //       headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       },
+  //     })
+  //     .then((res) => res.data);
+  //   return data;
+  // }, [data1]);
 
-  console.log(data1);
-  useEffect(() => {
-    if (token) {
-      async function getProfile() {
-        let token1 = await getToken();
-        console.log(token1.access_token);
-        let profile = await axios
-          .get("https://www.discord.com/api/v10/users/@me", {
-            headers: { Authorization: `Bearer ${token1.access_token}` },
-          })
-          .then((res) => res.data);
-        console.log(profile);
-      }
+  // console.log(data1);
+  // useEffect(() => {
+  //   if (token) {
+  //     async function getProfile() {
+  //       let token1 = await getToken();
+  //       console.log(token1.access_token);
+  //       let profile = await axios
+  //         .get("https://www.discord.com/api/v10/users/@me", {
+  //           headers: { Authorization: `Bearer ${token1.access_token}` },
+  //         })
+  //         .then((res) => res.data);
+  //       console.log(profile);
+  //     }
 
-      getProfile();
-    }
-  }, [getToken, token]);
+  //     getProfile();
+  //   }
+  // }, [getToken, token]);
 
   return (
     <div>
@@ -77,23 +79,20 @@ function ProfilePage() {
         </div>
 
         <div className="text-white mt-10">
-          <h1>Connect Accounts</h1>
+          <h1>Connect Account</h1>
           <div className="flex flex-wrap gap-3 mt-5">
-            <a
+            {/* <a
               href="/"
               className="flex gap-2 rounded-full px-8 py-2 items-center outline outline-5 outline-red-400"
             >
               <FaTwitter /> Twitter
-            </a>
-            <a
-              href="https://discord.com/api/oauth2/authorize?client_id=992266095601909840&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile&response_type=code&scope=identify%20email"
-              className="flex gap-2 rounded-full px-8 py-2 items-center outline outline-5 outline-red-400"
-            >
-              <FaDiscord /> Discord
-            </a>
+            </a> */}
+
+            <Button onClick={signInwithGoogle}>
+              <FaGoogle /> Gmail
+            </Button>
           </div>
         </div>
-
         <div className="text-white mt-10">
           <h1>Recent History</h1>
           <img className="block max-w-full" alt="" src={loader} />
