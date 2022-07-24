b119471fa1ef8992c16c1e1f101bba0881fa93d9
bolaji ogidan <bolajiscott@yahoo.com>
2022-07-24 23:20:56 +0100

    added-image

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,16 @@
+import phone from "../assets/phone.png";
 const Home = () => {
-  return <div>learning react</div>;
+  return (
+    <>
+      <h1>
+        DECENTRALISED <br />
+        PAYMENT SYSTEM WITH <br />
+        TRON
+      </h1>
+      <aside>
+        <img src={phone} width={"600px"}></img>
+      </aside>
+    </>
+  );
 };
 export default Home;
