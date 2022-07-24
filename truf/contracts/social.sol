6c282b6d61b65497c1f57a797318f8f4e77b21ad
mumeen <azeezmumeen6@gmail.com>
2022-07-24 05:47:12 +0100

    made ammendments to the smart contract and forntend

--- truf/contracts/social.sol
+++ truf/contracts/social.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.0;
 
 contract Trasaction {
     //transfer events
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
     event Transfer(address from, address to, string note, uint256 timestamp);
 
-    //user object
-    struct userObject {
-        address walletAddress;
-        string userName;
-        string twitterHandle;
-        string emailAddress;
-        string profileImg;
-    }
+    uint256 txnCounter;
+
     //transaction object
     struct transactionObject {
         address senderAddress;
@@ -23,12 +23,36 @@ contract Trasaction {
         string asset;
     }
 
+    //transaction object
+    struct userTransaction {
+        uint256 id;
+        string status;
+        uint256 index;
+        address senderAddress;
+        address ReceiverAddress;
+        uint256 Amount;
+        uint256 timestamp;
+        string note;
+        string asset;
+        string direction;
+    }
+    userTransaction[] txArray;
+
+    //user object
+    struct userObject {
+        address walletAddress;
+        string userName;
+        string twitterHandle;
+        string emailAddress;
+        string profileImg;
+    }
+
     //users map
     userObject[] public userArray;
     mapping(address => userObject) public userMap;
 
-    //transactions
-    transactionObject[] public transactionsList;
+    // //transactions
+    userTransaction[] transactionsList;
 
     // adding user on connection with Email
     function addUser(
@@ -36,9 +60,10 @@ contract Trasaction {
         string memory _user,
         string memory _handle,
         string memory _email,
-        string calldata image
+        string memory image
     ) public {
         require(userMap[_address].walletAddress != _address, "User Exists");
+
         userObject memory user = userObject(
             _address,
             _user,
@@ -61,20 +86,38 @@ contract Trasaction {
         address _senderAddress,
         address _ReceiverAddress,
         uint256 _Amount,
-        uint256 timestamp,
         string calldata _note,
         string calldata asset
     ) private {
+        txnCounter += 1;
+        uint256 index = transactionsList.length;
         transactionsList.push(
-            transactionObject(
+            userTransaction(
+                txnCounter,
+                "pending",
+                index,
                 _senderAddress,
                 _ReceiverAddress,
                 _Amount,
-                timestamp,
+                block.timestamp,
                 _note,
-                asset
+                asset,
+                "out"
             )
         );
+        // userMap[_ReceiverAddress].transactions.push(
+        //     userTransaction(
+        //         txnCounter,
+        //         "pending",
+        //         _senderAddress,
+        //         _ReceiverAddress,
+        //         _Amount,
+        //         block.timestamp,
+        //         _note,
+        //         asset,
+        //         "in"
+        //     )
+        // );
     }
 
     // get a particular user on login
@@ -97,16 +140,41 @@ contract Trasaction {
         string calldata note,
         string calldata asset
     ) public payable {
-        addTxn(sender, _receiver, _amount, block.timestamp, note, asset);
+        addTxn(sender, _receiver, _amount, note, asset);
         emit Transfer(sender, _receiver, note, block.timestamp);
     }
 
-    //get transactions
-    function getTransactions()
-        public
-        view
-        returns (transactionObject[] memory)
-    {
-        return transactionsList;
+    //claim fund
+    function claimTxn(
+        address payable reciever,
+        uint256 id,
+        uint256 amt
+    ) public payable {
+        if (
+            transactionsList[id].ReceiverAddress == msg.sender &&
+            keccak256(abi.encodePacked(transactionsList[id].status)) ==
+            keccak256(abi.encodePacked("pending"))
+        ) {
+            reciever.transfer(amt);
+            transactionsList[id].status = "completed";
+        }
+    }
+
+    function undoTxn(
+        address payable sender,
+        uint256 id,
+        uint256 amt
+    ) public payable {
+        require(
+            (transactionsList[id].senderAddress == msg.sender),
+            "Unathorized"
+        );
+        require(
+            (keccak256(abi.encodePacked(transactionsList[id].status)) ==
+                keccak256(abi.encodePacked("pending"))),
+            "Invalid Attempt"
+        );
+        sender.transfer(amt);
+        transactionsList[id].status = "completed";
     }
 }
